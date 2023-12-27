ff564a0455061eddfab7f67babe71888cef93079
Migwel <blog@migwel.dev>
2023-12-27 23:25:19 +0100

    Add rawData to the Song model

--- src/main/java/dev/migwel/sts/model/Song.java
+++ src/main/java/dev/migwel/sts/model/Song.java
@@ -1,3 +1,12 @@
 package dev.migwel.sts.model;
 
-public record Song(String artist, String title) {}
+/**
+ * Representation of a song playing "somewhere"
+ *
+ * @param artist The artist performing that song
+ * @param title The title of the number
+ * @param rawData It can happen that parsing the artist and title can be tricky and what we have is
+ *     raw data of what's playing. We may want this raw data either to save it and parse it manually
+ *     later or for debugging reasons
+ */
+public record Song(String artist, String title, String rawData) {}
